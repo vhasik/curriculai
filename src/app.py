4b331d95537da6e2dccad2c7ea2f6460b06a9ae6
Vaclav-BT <70612263+vhasik@users.noreply.github.com>
2023-11-05 12:18:14 -0800

    added loader, removed header

--- src/app.py
+++ src/app.py
@@ -44,18 +44,65 @@ colors = {
 # fmt: on
 
 
-def print_layout_for_day(day):
+# def print_layout_for_day(day):
+#     day_id = day.lower()[:3]
+#     return dmc.Grid([
+#         dmc.Col(html.Div([
+#             dmc.Text(f"{day}:", size="lg", color=colors["heading1"], underline=True),
+#             html.Div(id=f"print-{day_id}-activity", style=style),
+#         ], style=style), span=4),
+#         dmc.Col(html.Div([
+#             dmc.Text(f"Skills:", size="lg", color=colors["heading2"], underline=True),
+#             html.Div(id=f"print-{day_id}-skills", style=style),
+#         ], style=style), span=4),
+#     ])
+
+def print_layout_for_day(day, day_color, activity_color, skills_label_color, skills_description_color):
     day_id = day.lower()[:3]
     return dmc.Grid([
         dmc.Col(html.Div([
-            dmc.Text(f"{day}:", size="lg", color=colors["heading1"], underline=True),
+            dmc.Text(f"{day}:", size="lg", color=day_color, underline=True, id=f"print-{day_id}-day"),
             html.Div(id=f"print-{day_id}-activity", style=style),
         ], style=style), span=4),
         dmc.Col(html.Div([
-            dmc.Text(f"Skills:", size="lg", color=colors["heading2"], underline=True),
+            dmc.Text(f"Skills:", size="lg", color=skills_label_color, underline=True, id=f"print-{day_id}-skills-label"),
             html.Div(id=f"print-{day_id}-skills", style=style),
         ], style=style), span=4),
     ])
+
+
+def print_layout(class_name, teachers, week_range, theme, activities, skills):
+    layout = dmc.Grid(
+        children=[
+            dmc.Col(html.Div([
+                dmc.Text("Class Name:", size="lg", color=colors["heading1"], underline=True),
+                html.Div(class_name, style=style),
+            ], style={"fontFamily": "Roboto"}), span=4),
+            dmc.Col(html.Div([
+                dmc.Text("Teachers:", size="lg", color=colors["heading1"], underline=True),
+                html.Div(teachers, style=style),
+            ], style=style), span=4),
+            dmc.Col(html.Div([
+                dmc.Text("Week Of:", size="lg", color=colors["heading1"], underline=True),
+                html.Div(week_range, style=style),
+            ], style=style), span=4),
+            dmc.Col(html.Div([
+                dmc.Text("Theme:", size="lg", color=colors["heading1"], underline=True),
+                html.Div(theme, style=style),
+            ], style=style), span=12),
+            dmc.Col(html.Div([
+                dmc.Text("Activities:", size="lg", color=colors["heading2"], underline=True),
+                html.Div(activities, style=style),
+            ], style=style), span=12),
+            dmc.Col(html.Div([
+                dmc.Text("Skills:", size="lg", color=colors["heading2"], underline=True),
+                html.Div(skills, style=style),
+            ], style=style), span=12),
+        ],
+        gutter="xl",
+        justify="center",
+    )
+    return layout
 
 
 def use_gpt(instructions, prompt_text):
@@ -88,7 +135,7 @@ def use_gpt(instructions, prompt_text):
 
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]  # Add more days if needed
 # day_layouts = [daily_activity(day) for day in days]
-print_layouts = [print_layout_for_day(day) for day in days]
+# print_layouts = [print_layout(day) for day in days]
 
 
 app = Dash(
@@ -120,7 +167,6 @@ header = dmc.Center(
 body = dmc.Tabs([
     dmc.TabsList([
         dmc.Tab("Curriculum", value="curriculum", icon=DashIconify(icon="tabler:book")),
-        # dmc.Tab("Skills", value="skills", icon=DashIconify(icon="tabler:star")),
         dmc.Tab("Print", value="print", icon=DashIconify(icon="tabler:printer")),
         dmc.Tab("Email", value="email", icon=DashIconify(icon="tabler:mail")),
     ], position="center",),
@@ -206,6 +252,7 @@ body = dmc.Tabs([
                 dmc.Textarea(
                     id="skills-text",
                     label="Skills:",
+                    style={"textAlign": "center"},
                     placeholder="Let ChatGPT generate skills for you! Click the button above.",
                     minRows=7,
                     autosize=True), ],
@@ -217,28 +264,52 @@ body = dmc.Tabs([
 
     # PRINT ====================================
     dmc.TabsPanel([
+        # Add color picker inputs
+        # dmc.Grid([
+        #     dmc.Col(html.Div([
+        #         html.Label('Day Label Color:'),
+        #         dmc.ColorPicker(id='day-color-picker', swatches=swatch1),  # updated
+        #     ], style=style), span=3),
+        #     dmc.Col(html.Div([
+        #         html.Label('Activity Description Color:'),
+        #         dmc.ColorPicker(id='activity-color-picker', swatches=swatch1),  # updated
+        #     ], style=style), span=3),
+        #     dmc.Col(html.Div([
+        #         html.Label('Skills Label Color:'),
+        #         dmc.ColorPicker(id='skills-label-color-picker', swatches=swatch1),  # updated
+        #     ], style=style), span=3),
+        #     dmc.Col(html.Div([
+        #         html.Label('Skills Description Color:'),
+        #         dmc.ColorPicker(id='skills-description-color-picker', swatches=swatch1),  # updated
+        #     ], style=style), span=3),
+        # ]),
+        # Rest of print area
         dmc.Space(h=20),
-        dmc.Grid([
-            dmc.Col(html.Div([
-                dmc.Text("Class Name:", size="lg", color=colors["heading1"], underline=True),
-                html.Div(id="print-class-name", style=style),  # Add this line
-            ], style={"fontFamily": "Roboto"}), span=4),
-            dmc.Col(html.Div([
-                dmc.Text("Teachers:", size="lg", color=colors["heading1"], underline=True),
-                html.Div(id="print-teachers", style=style),  # Add this line
-            ], style=style), span=4),
-            dmc.Col(html.Div([
-                dmc.Text("Week Of:", size="lg", color=colors["heading1"], underline=True),
-                html.Div(id="print-week-of", style=style),  # Add this line
-            ], style=style), span=4),
-            dmc.Col(html.Div([
-                dmc.Text("Theme:", size="lg", color=colors["heading1"], underline=True),
-                html.Div(id="print-theme", style=style),  # Add this line
-            ], style=style), span=12),
-        ]),
-        dmc.Space(h=20),
-        *print_layouts,  # Unpack the list of print layouts
-        dmc.Space(h=20),
+        # dmc.Grid([
+        #     dmc.Col(html.Div([
+        #         dmc.Text("Class Name:", size="lg", color=colors["heading1"], underline=True),
+        #         html.Div(id="print-class-name", style=style),  # Add this line
+        #     ], style={"fontFamily": "Roboto"}), span=4),
+        #     dmc.Col(html.Div([
+        #         dmc.Text("Teachers:", size="lg", color=colors["heading1"], underline=True),
+        #         html.Div(id="print-teachers", style=style),  # Add this line
+        #     ], style=style), span=4),
+        #     dmc.Col(html.Div([
+        #         dmc.Text("Week Of:", size="lg", color=colors["heading1"], underline=True),
+        #         html.Div(id="print-week-of", style=style),  # Add this line
+        #     ], style=style), span=4),
+        #     dmc.Col(html.Div([
+        #         dmc.Text("Theme:", size="lg", color=colors["heading1"], underline=True),
+        #         html.Div(id="print-theme", style=style),  # Add this line
+        #     ], style=style), span=12),
+        # ]),
+        # dmc.Space(h=20),
+        # # *print_layouts,  # Unpack the list of print layouts
+        # dmc.Space(h=20),
+        # html.Div(id='print-layouts-container'),  # Add this div to hold the print_layouts
+
+        # Simplified print area
+        html.Div(id='print-layout-output'),
     ], value="print"),
 
     # EMAIL ====================================
@@ -254,17 +325,11 @@ body = dmc.Tabs([
                 size="sm",
                 fullWidth=False,
                 leftIcon=DashIconify(icon="ri:openai-fill"),
-            ),
-            dmc.Loader(color="indigo", size="sm", variant="dots", id="loader-email", style={"display": "none"}),
-            html.Div([
-                dmc.Text("thinking, please wait..."),],
-                id="loading-email",
-                style={"display": "block"}  # Initially hidden
             ),],),
         dmc.Space(h=10),
         dcc.Loading(
             id="loading-email-text",
-            type="default",
+            type="circle",
             children=[
                 dmc.Textarea(
                     id="email-text",
@@ -312,24 +377,65 @@ app.layout = dmc.MantineProvider(
 )
 
 
+# @callback(
+#     Output("print-class-name", "children"),
+#     Output("print-teachers", "children"),
+#     Output("print-week-of", "children"),
+#     Output("print-theme", "children"),
+#     # Additional Outputs for day, activity, skills label, and skills text
+#     [Output(f"print-{day.lower()[:3]}-day", "style") for day in days] +
+#     [Output(f"print-{day.lower()[:3]}-activity", "style") for day in days] +
+#     [Output(f"print-{day.lower()[:3]}-skills-label", "style") for day in days] +
+#     [Output(f"print-{day.lower()[:3]}-skills", "style") for day in days],
+#     Input("class-name", "value"),
+#     Input("teachers", "value"),
+#     Input("week-of", "date"),
+#     Input("theme", "value"),
+#     # Additional Inputs for the color pickers
+#     Input("day-color-picker", "value"),
+#     Input("activity-color-picker", "value"),
+#     Input("skills-label-color-picker", "value"),
+#     Input("skills-description-color-picker", "value"),
+# )
+# def update_print_tab(
+#         class_name, teachers, week_of, theme,
+#         day_color, activity_color, skills_label_color, skills_description_color
+# ):
+#     print_layouts = [print_layout(day, day_color, activity_color, skills_label_color, skills_description_color) for day in days]
+#
+#     # Set the color styles for the day, activity, skills label, and skills text
+#     day_style = {"color": day_color["hex"]}
+#     activity_style = {"color": activity_color["hex"]}
+#     skills_label_style = {"color": skills_label_color["hex"]}
+#     skills_style = {"color": skills_description_color["hex"]}
+#
+#     return (
+#         class_name, teachers, week_of, theme,
+#         # Return the color styles for each day and text element
+#         *[day_style] * len(days),
+#         *[activity_style] * len(days),
+#         *[skills_label_style] * len(days),
+#         *[skills_style] * len(days),
+#         print_layouts
+#     )
+
+
 @callback(
-    Output("print-class-name", "children"),
-    Output("print-teachers", "children"),
-    Output("print-week-of", "children"),
-    Output("print-theme", "children"),
+    Output("print-layout-output", "children"),
     Input("class-name", "value"),
     Input("teachers", "value"),
     Input("week-of", "date"),
     Input("theme", "value"),
-    # Input('activities', 'value'),
-    # Input('skills-text', 'value'),
-    # Input('day-color-picker', 'value'),
-    # Input('activity-color-picker', 'value'),
-    # Input('skills-label-color-picker', 'value'),
-    # Input('skills-description-color-picker', 'value')
+    Input("activities", "value"),
+    Input("skills-text", "value"),
 )
-def update_print_tab(class_name, teachers, week_of, theme):
-    return class_name, teachers, week_of, theme
+def update_print_tab(class_name, teachers, week_of, theme, activities, skills):
+    if week_of is None:
+        week_range = "Unknown"
+    else:
+        week_range = f"{week_of.strftime('%B %d')} - {date.fromisoformat(week_of) + pd.Timedelta(days=4):%B %d, %Y}"
+    layout = print_layout(class_name, teachers, week_range, theme, activities, skills)
+    return layout
 
 
 @callback(
@@ -346,10 +452,10 @@ def generate_skills(n_clicks, activities_text):
     return no_update
 
 
-@app.callback(
+@callback(
     Output("email-text", "value"),  # Output for the loader's style
-    [Input("generate-email", "n_clicks"),
-     Input("activities", "value")]
+    Input("generate-email", "n_clicks"),
+    Input("activities", "value")
 )
 def generate_email(n_clicks, activities_text):
     if n_clicks:
